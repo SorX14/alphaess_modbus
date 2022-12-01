5be13614c44e0cae9234d0166661f3011d07ba01
steve.parker <steve.parker43@gmail.com>
2022-12-01 22:12:17 +0000

    Remove redundant code

--- alphaess_modbus/readertcp.py
+++ alphaess_modbus/readertcp.py
@@ -1,16 +1,11 @@
-import asyncio
 from pymodbus.client import AsyncModbusTcpClient
 from pymodbus.payload import BinaryPayloadDecoder
 import json
-from .formatter import Formatter
 from .reader import Reader
 from pathlib import Path
 
 class ReaderTCP(Reader):
     instrument: AsyncModbusTcpClient
-    mapping: list
-    custom_formatter = None
-    debug: bool
     slave_id: int
 
     def __init__(self, ip=None, port=502, slave_id=int(0x55), debug=False, json_file=None, formatter=None) -> None:
@@ -31,17 +26,6 @@ class ReaderTCP(Reader):
             self.mapping = json.load(f)
         except OSError:
             raise RuntimeError(f"Could not find JSON file {json_file!r}")
-
-    def conform_name(self, name) -> str:
-        """ Conform a register name if copied from PDF """
-        return name.lower().strip().replace("  ", " ").translate({ord(ch): "_" for ch in ' (-:'}).translate(
-            {ord(ch): None for ch in ')'}).replace("___", "_").replace("__", "_")
-
-    async def get_units(self, name) -> str:
-        """ Get the units (e.g. 'KWh') for a register if available """
-        name = self.conform_name(name)
-        register = await self.get_definition(name)
-        return register['units']
 
     async def get_value(self, name) -> int:
         """ Ask inverter for a specific register """
@@ -73,44 +57,3 @@ class ReaderTCP(Reader):
             val = val / float(divisor)
 
         return val
-
-    async def get_formatted_value(self, name, use_formatter=True):
-        """
-        Ask inverter for a specific register and add units (if available).
-        Normally returns a str but can be overridden with formatters
-        """
-        name = self.conform_name(name)
-        val = await self.get_value(name)
-
-        if use_formatter is True:
-            # Use custom formatter first
-            if self.custom_formatter is not None:
-                if callable(getattr(self.custom_formatter, name, None)):
-                    func = getattr(self.custom_formatter, name)
-                    return func(self.custom_formatter, val)
-
-            # Now try internal formatter
-            if callable(getattr(Formatter, name, None)):
-                func = getattr(Formatter, name)
-                return func(Formatter, val)
-
-        units = await self.get_units(name)
-        return f"{val}{units}"
-
-    async def get_definition(self, name) -> dict:
-        """ Look up name in JSON registers """
-        name = self.conform_name(name)
-        register = next((item for item in self.mapping if item["name"] == name), None)
-        if register is None:
-            raise RuntimeError(f"Register name not found: {name!r}")
-
-        # Sanity check the result
-        try:
-            assert "name" in register
-            assert "type" in register
-            assert "decimals" in register
-            assert "units" in register
-        except AssertionError as e:
-            raise RuntimeError(f"JSON file has a malformed entry for {name}")
-
-        return register
